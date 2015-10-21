fd0d64409c46eaac61a31859bf75091f48570d54
remis1889 <remis1889@gmail.com>
2015-10-21 16:31:30 +0300

    Navigation
    added navigation

--- js/calendar.js
+++ js/calendar.js
@@ -1,85 +1,80 @@
-// these are labels for the days of the week
-cal_days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-// these are human-readable month name labels, in order
-cal_months_labels = ['January', 'February', 'March', 'April',
+months_labels = ['January', 'February', 'March', 'April',
                      'May', 'June', 'July', 'August', 'September',
                      'October', 'November', 'December'];
 
-// these are the days of the week for each month, in order
-cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-// this is the current date
-cal_current_date = new Date(); 
+current_date = new Date(); 
 
 function change(month,year) {
+
 	if(month < 0) {
+    nextMonth = 0;
+    nextYear = year
+
 		month = 11;
 		year = year - 1;
 	}
-	else if(month > 11){
-		month = 0;
-		year = year + 1;
-	}
-	
-	var newCal = new Calendar(month, year);
-	newCal.generateHTML();
-	document.getElementById('leaveCal').innerHTML = newCal.getHTML();
+	else { 
+    if(month > 11){ 
+    		month = 0;
+		    year = year + 1; 
+      }
+    nextMonth = month + 1;
+    nextYear = year;
+
+    if (nextMonth > 11) {
+      nextMonth = 0;
+      nextYear += 1;
+    }
+  }
+  var cal = new Calendar(month,year);
+  var nextCal = new Calendar(nextMonth,nextYear);
+
+  cal.generateHTML();
+  nextCal.generateHTML();
+
+  document.getElementById('cal-table').innerHTML = cal.getHTML();
+  document.getElementById('cal-table').innerHTML += nextCal.getHTML();
+
+  document.getElementById('prev').innerHTML = '<a href="javascript:void(0);" onclick="return change('+(cal.month-1)+',' + cal.year + ');"><div id="sel-prev"></div></a>';
+  document.getElementById('next').innerHTML = '<a href="javascript:void(0);" onclick="return change('+(nextCal.month)+',' + nextCal.year + ');"><div id="sel-next"></div></a>';
 
 }
 
 function Calendar(month, year) {
-  this.month = (isNaN(month) || month == null) ? cal_current_date.getMonth() : month;
-  this.year  = (isNaN(year) || year == null) ? cal_current_date.getFullYear() : year;
+  this.month = month;
+  this.year  = year;
   this.html = '';
 }
 
 Calendar.prototype.generateHTML = function(){
 
-  // get first day of month
   var firstDay = new Date(this.year, this.month, 1);
   var startingDay = firstDay.getDay();
+  var monthLength = days_in_month[this.month];
+  var monthName = months_labels[this.month];
 
-  // switch to next year after December
-
-    if(this.month + 1 > 11){
-		nextMonth = 0;
-		nextYear = this.year + 1;
-	}
-	else{
-		nextMonth = this.month + 1;
-		nextYear = this.year;
-	}
-  console.log(nextMonth,nextYear);
-  
-  // find number of days in month
-  var monthLength = cal_days_in_month[this.month];
-  var nextMonthLength = cal_days_in_month[nextMonth];
-
-  
   // compensate for leap year
   if (this.month == 1) { // February only!
     if((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0){
       monthLength = 29;
     }
   }
-  
-  // do the header
-  var monthName = cal_months_labels[this.month]
-  var nextMonthName = cal_months_labels[nextMonth]
 
-  var html = '<div class = "sel-arrow">';
-  html += '<a href="javascript:void(0);" onclick="return change('+(this.month-1)+',' + this.year + ');"><div id="sel-prev"></div></a></div>';
-  html += '<div class="cal-table"> <table><tr id="month_row">';
-  html += '<th colspan="'+ monthLength +'">' + monthName + "&nbsp;" + this.year + '</th>';
-  html += '<th colspan="'+ nextMonthLength +'">' + nextMonthName + "&nbsp;" + nextYear +'</th></tr>';
+// month header
+
+  var html = '<table><tr id="month_row">';
+  html += '<th colspan="'+ monthLength +'">' + monthName + "&nbsp;" + this.year + '</th></tr>';
   html += '<tr class="calendar-header">';
-  
 
-  for(var i = startingDay, j = 0; j < (monthLength + nextMonthLength); i++, j++ ){
-    
+// day labels
+
+  for(var i = startingDay, j = 0; j < monthLength; i++, j++ ){
     html += '<td class="calendar-header-day">';
-    html += cal_days_labels[i];
+    html += days_labels[i];
     html += '</td>';
 
     if(i == 6){
@@ -90,36 +85,20 @@ Calendar.prototype.generateHTML = function(){
 
   // fill in the days
 
-  html += '<tr class = "days-row">'
-
+  html += '<tr class = "days-row">';
   for(var i = 1; i <= monthLength; i++) {
   	html += '<td class="calendar-day">' + i + '</td>';
   }
 
-  for(var i = 1; i <= nextMonthLength; i++) {
-  	html += '<td class="calendar-day">' + i + '</td>';
-  }
-
-  html += '</tr></table></div></div><div class = "sel-arrow">';
-  html += '<a href="javascript:void(0);" onclick="return change('+(nextMonth)+',' + nextYear + ');"><div id="sel-next"></div></a></div>';
-
+  html += '</tr></table>';
   this.html = html;
-
- 
 }
-
-
 
 Calendar.prototype.getHTML = function() {
   return this.html;
 }   
 
-var cal = new Calendar();
 
-cal.generateHTML();
-
-window.onload  =   function() {
-    document.getElementById('leaveCal').innerHTML = cal.getHTML();
-}
-
-
+$( document ).ready(function() {
+  change(current_date.getMonth(),current_date.getFullYear());  
+});
--- js/main.js
+++ js/main.js
@@ -3,15 +3,82 @@ currentWindow = gui.Window.get();
 
 var windowMenu = new gui.Menu({ type: 'menubar' });
 
-// Add first level menu
 var firstMenuItem = new gui.MenuItem({
-label:'File',
+label:'Admin',
 submenu: new gui.Menu()
 });
+
 windowMenu.append(firstMenuItem);
+
+firstMenuItem.submenu.append(new gui.MenuItem({
+	label:'New Employee', 
+	click : function () {
+          gui.Window.open('add_emp.html', {
+          	position: 'center',
+          	width: 640,
+          	height: 480,
+          	focus: true
+          });
+     }
+  })
+);
+
+firstMenuItem.submenu.append(new gui.MenuItem({
+	label:'Schedule Leave', 
+	click : function () {
+          gui.Window.open('schedule.html', {
+          	position: 'center',
+          	width: 640,
+          	height: 480,
+          	focus: true
+          });
+      }
+  })
+);
+
+firstMenuItem.submenu.append(new gui.MenuItem({	type:'separator'  }));
+
+firstMenuItem.submenu.append(new gui.MenuItem({
+	label:'Exit', 
+	click : function () {
+          currentWindow.close();
+      }
+  })
+);
+currentWindow.menu = windowMenu;
+
+
+var secondMenuItem = new gui.MenuItem({
+label:'View',
+submenu: new gui.Menu()
+});
+windowMenu.append(secondMenuItem);
 // Add submenu items
-firstMenuItem.submenu.append(new gui.MenuItem({label:'Open'}));
-firstMenuItem.submenu.append(new gui.MenuItem({label:'Save'}));
-firstMenuItem.submenu.append(new gui.MenuItem({type:'separator'}));
-firstMenuItem.submenu.append(new gui.MenuItem({label:'Exit'}));
-gui.Window.get().menu = windowMenu;
+secondMenuItem.submenu.append(new gui.MenuItem({
+	label:'View Employees', 
+	click : function () {
+          gui.Window.open('view_emp.html', {
+          	position: 'center',
+          	width: 640,
+          	height: 480,
+          	focus: true
+          });
+      }
+  })
+);
+
+
+secondMenuItem.submenu.append(new gui.MenuItem({
+	label:'Export Schedule', 
+	click : function () {
+          gui.Window.open('export.html', {
+          	position: 'center',
+          	width: 640,
+          	height: 480,
+          	focus: true
+          });
+      }
+  })
+);
+currentWindow.menu = windowMenu;
+
